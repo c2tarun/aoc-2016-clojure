9b70e1f467b501e8dec8e160b7537737237863ee
Tarun K. Mall <c2tarun@gmail.com>
2023-01-28 09:31:30 -0800

    Day 3 part 1 done.

---
+++ src/aoc_2016_clojure/day3.clj
@@ -0,0 +1,21 @@
+(ns aoc-2016-clojure.day3 (:require [clojure.string :as str]))
+
+(defn is-triangle? [sides]
+  (let [sides (sort sides)]
+    (> (reduce + (take 2 sides)) (last sides))))
+
+(defn to-int [str-array]
+  (map #(read-string %) str-array))
+
+(to-int ["1" "2" "3"])
+
+(def input (slurp "resources/day3-input.txt"))
+
+(def triangles (as-> input x
+                 (str/split x #"\n")
+                 (map str/trim x)
+                 (map #(str/split % #"\s\s*") x)
+                 (map to-int x)
+                 (filter is-triangle? x)))
+
+(count triangles)
