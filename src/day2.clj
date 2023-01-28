6944b2216c7677cf11da0fb1728a464bb4d62078
Tarun K. Mall <c2tarun@gmail.com>
2023-01-28 08:05:18 -0800

    Part2 of day 2 done.

--- src/day2.clj
+++ src/day2.clj
@@ -1,23 +1,32 @@
 (ns day2 (:require [clojure.string :as str]))
 
-(def keypad [[1 2 3] [4 5 6] [7 8 9]])
-
-(defn move
-  "Method returns the coordinate based on current coordinates and move direction.
-   If move step cannot be performed it'll return the same coordinate."
-  ([[r c] dir]
-   (move [r c] dir (count (get keypad 0))))
-  ([[r c] dir size]
-   (cond
-     (= dir \U) [(max (dec r) 0) c]
-     (= dir \D) [(min (inc r) (dec size)) c]
-     (= dir \L) [r (max (dec c) 0)]
-     (= dir \R) [r (min (inc c) (dec size))])))
+(def keypad [[:e :e :e :e :e :e]
+             [:e :e :e 1 :e :e :e]
+             [:e :e  2 3 4  :e :e]
+             [:e 5   6 7 8   9 :e]
+             [:e :e \A \B \C :e :e]
+             [:e :e :e \D :e :e :e]
+             [:e :e :e :e :e :e :e]])
 
 (defn get-code
   "Returns the keypad value of the given coordinate."
   [coord]
   (get-in keypad coord))
+
+(def action {\U [-1 0]
+             \D [1 0]
+             \L [0 -1]
+             \R [0 1]})
+
+(defn shift [[r c] dir]
+  (let [[ar ac] (get action dir)]
+    [(+ r ar) (+ c ac)]))
+
+(defn move [[r c] dir]
+  (let [new-coord (shift [r c] dir)]
+    (if (= :e (get-code new-coord))
+      [r c]
+      new-coord)))
 
 (def input (slurp "resources/day2-input.txt"))
 
@@ -26,15 +35,13 @@
   ([start line]
    (reduce (fn [coord dir] (move coord dir)) start line)))
 
-(find-button `(\U \L \L))
-
 (defn clean-input [input]
   (as-> input x
     (str/split x #"\n")
-    (map str/trim x)
-    (map seq x)))
+    (mapv str/trim x)
+    (mapv seq x)))
 
-(loop [input (clean-input input) current [1 1] final-code []]
+(loop [input (clean-input input) current [3 1] final-code []]
   (if (empty? input)
     final-code
     (let [next (find-button current (first input))]
