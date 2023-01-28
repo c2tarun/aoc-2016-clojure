512042e36ff5349fe3780b34405903808cc530dc
Tarun K. Mall <c2tarun@gmail.com>
2023-01-28 10:03:17 -0800

    Day 3 part 2 done.

--- src/aoc_2016_clojure/day3.clj
+++ src/aoc_2016_clojure/day3.clj
@@ -7,14 +7,27 @@
 (defn to-int [str-array]
   (mapv #(read-string %) str-array))
 
-(to-int ["1" "2" "3"])
+(defn clean-input [filename]
+  (let [input (slurp filename)]
+    (as-> input x
+      (str/split x #"\n")
+      (map str/trim x)
+      (map #(str/split % #"\s\s*") x)
+      (map to-int x))))
 
-(def input (slurp "resources/day3-input.txt"))
-
-(as-> input x
-  (str/split x #"\n")
-  (map str/trim x)
-  (map #(str/split % #"\s\s*") x)
-  (map to-int x)
+;; Part 1
+(as-> (clean-input "resources/day3-input.txt") x
   (filter is-triangle? x)
   (count x))
+
+;; Part 2
+(defn count-triangles-in-grid [[r1 r2 r3]]
+  (as-> (range 3) x
+    (map (fn [x] [(get r1 x) (get r2 x) (get r3 x)]) x)
+    (filter is-triangle? x)
+    (count x)))
+
+(as-> (clean-input "resources/day3-input.txt") x
+  (partition 3 x)
+  (map #(count-triangles-in-grid %) x)
+  (reduce + x))
